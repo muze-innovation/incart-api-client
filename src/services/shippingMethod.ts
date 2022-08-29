f4ea4552841113522b8bfac4217a03f9494b9118
Bee <thammawat@muze.co.th>
2022-08-29 10:09:31 +0700

    fix meta type and icart word

--- src/services/shippingMethod.ts
+++ src/services/shippingMethod.ts
@@ -1,9 +1,9 @@
 import { InCartService } from './client'
-import { IncartShippingMethod } from './models/shippingMethod'
+import { InCartShippingMethod } from './models/shippingMethod'
 
 
 export class InCartShippingMethodService extends InCartService {
-  public async list(storeId: string): Promise<{ collection: IncartShippingMethod[] }> {
+  public async list(storeId: string): Promise<{ collection: InCartShippingMethod[] }> {
     const res = await this.axios.get(`/pcms/${storeId}/api/v1/shipping-methods/admin`)
     return res.data
   }
