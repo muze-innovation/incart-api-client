1f425a8392a0c44d537fe031af177e014a1da824
Kittiphat S <freeuxer@gmail.com>
2022-08-29 10:28:38 +0700

    added shipping method types

--- src/services/models/shippingMethod.ts
+++ src/services/models/shippingMethod.ts
@@ -1,25 +1,61 @@
 import { LangPair } from "./base";
 
-export interface InCartShippingMethod {
-  id: number
-  isActive: boolean
-  isDefault: boolean
-  condition: {
-    initial: number
-    next: number
-    allowCountries: {
-      allowAll: boolean
-      countryList: string[]
-    }
-    COD: {
-      allow: boolean
-      maximumPrice: number | null
-      minimumPrice: number | null
-    }
+/**
+ * Supported shipping method types from inCart
+ * 
+ * @refs `incart-brand-next/shared/constants/shipping.js`
+ */
+export type InCartShippingMethodType = 'flatrate' | 'byItem' | 'byProvince' | 'byDistance' | 'jnt' | 'pickup' | 'kerry' | 'tablerate'
+
+interface InCartShippingMethodCondition {
+  initial: number
+  next: number
+  allowCountries: {
+    allowAll: boolean
+    countryList: string[]
   }
+  COD: {
+    allow: boolean
+    maximumPrice: number | null
+    minimumPrice: number | null
+  }
+}
+
+export interface InCartShippingMethod {
+  /**
+   * InCart's Shipping Method's ID.
+   */
+  id: number
+  /**
+   * Indicator for being Active.
+   */
+  isActive: boolean
+  /**
+   * Indicator for being default shipping method.
+   */
+  isDefault: boolean
+  /**
+   * 
+   */
+  condition: InCartShippingMethodCondition
+  /**
+   * Descriptive Text for the Method
+   */
   description: LangPair
+  /**
+   * Name of the Method
+   */
   name: LangPair
-  shippingType: string
+  /**
+   * Associated type of this Method.
+   */
+  shippingType: InCartShippingMethodType
+  /**
+   * Sorting order to be displayed on front-end
+   */
   sortOrder: number
+  /**
+   * Tracking URL pattern.
+   */
   trackingUrl: string
 }
